69b88d23db7afec87eac9963c164c02695a54889
dmytryy <dmytrash.yura@gmail.com>
2019-10-04 12:35:45 -0700

    adding form fileds in StreamCreate

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
+
+  render() {
+    return (
+      <form>
+        <Field name="Title" component={this.renderInput} />
+        <Field name="Description" component={this.renderInput} />
+
+      </form>
+    );
+  }
 }
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
