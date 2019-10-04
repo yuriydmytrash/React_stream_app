a4bc80583410481e587e34f48bf87b60c779a941
dmytryy <dmytrash.yura@gmail.com>
2019-10-04 12:44:11 -0700

    customizing form fields

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput({ input }) {
-    return <input {...input} />;
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label >{label}</label>
+        <input {...input} />
+      </div>
+    );
   }
 
   render() {
     return (
-      <form>
-        <Field name="Title" component={this.renderInput} />
-        <Field name="Description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="Title" component={this.renderInput} label="Enter Title" />
+        <Field name="Description" component={this.renderInput} label="Enter Description" />
 
       </form>
     );
