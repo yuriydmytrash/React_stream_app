5faffa0406aa4709bd48052ff69ca1d17637d1ff
dmytryy <dmytrash.yura@gmail.com>
2019-10-08 14:50:32 -0700

    adding a link to streamList

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -28,7 +28,10 @@ class StreamList extends React.Component {
         <div className="item" key={stream.id}>
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
-          <div className="content">{stream.title}
+          <div className="content">
+            <Link to={`/streams/${stream.id}`} className="header">
+              {stream.title}
+            </Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
